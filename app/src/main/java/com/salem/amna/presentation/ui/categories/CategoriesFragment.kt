a162baf4f541418cb2b50d1d40d5c6e7a3692c85
Salim Badiea <37668158+SalimBadea@users.noreply.github.com>
2023-04-15 01:33:50 +0200

    categories, earnings, courses designs

---
+++ app/src/main/java/com/salem/amna/presentation/ui/categories/CategoriesFragment.kt
@@ -0,0 +1,44 @@
+package com.salem.amna.presentation.ui.categories
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.salem.amna.R
+import com.salem.amna.base.BaseFragment
+import com.salem.amna.databinding.FragmentCategoriesBinding
+
+class CategoriesFragment : BaseFragment() {
+
+    private val binding: FragmentCategoriesBinding by lazy {
+        FragmentCategoriesBinding.inflate(layoutInflater)
+    }
+
+    override fun getRootView(): View {
+        return binding.root
+    }
+
+    override fun initVar() {
+    }
+
+    override fun onEvent() {
+    }
+
+    override fun render() {
+    }
+
+    override fun navigate() {
+    }
+
+    override fun showEffect() {
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = CategoriesFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
